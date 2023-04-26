80ddbeb173556821c7c5b17a9f4a2d0a1d1699a7
Edwin P <edwin.rea3@gmail.com>
2023-04-26 19:50:06 -0300

    Pruebas

--- 01-DART-INTRO/02_variables.dart
+++ 01-DART-INTRO/02_variables.dart
@@ -1,32 +1,27 @@
 void main() {
   
   final String pokemon = 'Ditto';
-  
   final int hp = 100;
-  
   final bool isAlive = true;
-  
-//   final abilities = <String>['impostor'];
-  final List<String>abilities = ['impostor'];
-  
+  final List<String> abilities = ['impostor'];
   final sprites = <String>['ditto/front.png','ditto/back.png'];
   
-//   dynamic == null   // Por defecto es nulo
-  
-  dynamic errorMessage = 'hola';
-  
-  errorMessage = 123;
-  
-  
-  
+// dynamic == null
+  dynamic errorMessage = 'Hola';
+  errorMessage = true;
+  errorMessage = [1,2,3,4,5,6];
+  errorMessage = { 1,2,3,4,5,6 };
+  errorMessage = () => true;
+  errorMessage = null;
+    
   print("""
-  
   $pokemon
   $hp
   $isAlive
   $abilities
   $sprites
-  
+  $errorMessage
   """);
-    
+  
+  
 }
