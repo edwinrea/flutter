13eefbca93f947e595bb48a64085c16be7683ba8
Edwin P <edwin.rea3@gmail.com>
2023-04-27 14:29:34 -0300

    test

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world_app/presentation/screens/counter_screent.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_screent.dart';
 
 void main() {
   runApp(const MyApp());
