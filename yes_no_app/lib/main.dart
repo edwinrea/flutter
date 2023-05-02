2a3c99042a4507d7e2ab55f52d9861b59f9d9c8b
Edwin P <edwin.rea3@gmail.com>
2023-05-02 14:27:12 -0300

    a

--- yes_no_app/lib/main.dart
+++ yes_no_app/lib/main.dart
@@ -1,8 +1,21 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:yes_no_app/config/theme/app_theme.dart';
 import 'package:yes_no_app/presentation/screens/chat/chat_screen.dart';
 
-void main() => runApp(const MyApp());
+// void main() => runApp(const MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  ByteData data = await PlatformAssetBundle().load('assets/ca/lets-encrypt-r3.pem');
+  SecurityContext.defaultContext.setTrustedCertificatesBytes(data.buffer.asUint8List());
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
