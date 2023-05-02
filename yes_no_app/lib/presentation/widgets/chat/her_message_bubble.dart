a8be91cda4e01b28dac525b613a226463e407589
Edwin P <edwin.rea3@gmail.com>
2023-05-02 14:11:45 -0300

    cambio http

--- yes_no_app/lib/presentation/widgets/chat/her_message_bubble.dart
+++ yes_no_app/lib/presentation/widgets/chat/her_message_bubble.dart
@@ -40,7 +40,7 @@ class _ImageBubble extends StatelessWidget {
     return ClipRRect(
         borderRadius: BorderRadius.circular(20),
         child: Image.network(
-          'https://yesno.wtf/assets/no/30-d37eee83c3c2180de4edb7da6fa9f5b7.gif',
+          'http://yesno.wtf/assets/no/30-d37eee83c3c2180de4edb7da6fa9f5b7.gif',
           width: size.width * 0.7,
           height: 150,
           fit: BoxFit.cover,
